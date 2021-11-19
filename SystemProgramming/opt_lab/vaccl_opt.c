915833523a8340e16f16c159e66b13c19a90511f
seoyoungnation <ourworlds19@gmail.com>
2021-11-19 19:31:39 +0900

    add subject

---
+++ SystemProgramming/opt_lab/vaccl_opt.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<limits.h>
+
+//#define MAX_NUM 1000
+#define MAX_NUM 1000000000
+//#define MAX_NUM INT_MAX
+//#define MAX_NUM 100000
+
+struct vec {
+	int len;
+	int *data;
+};
+typedef struct vec vec_t;
+
+int* get_vec_start(vec_t* v){
+	return v->data;
+}
+
+int vec_length(vec_t* v)
+{
+	assert(v);
+	return v->len;
+}
+
+void combine(vec_t* v, int *dest)
+{
+	assert(dest);
+	//*dest = 0;
+	int i;
+	int len = vec_length(v); //new
+	int *d = get_vec_start(v);
+	int temp = 0;
+	for(i = 0; i < len; i++){
+		temp = temp + d[i];
+	}
+	*dest = temp;
+}
+
+void init(vec_t* v)
+{
+	assert(v);
+	v->len = MAX_NUM;
+	v->data = (int*) malloc(sizeof(int) * MAX_NUM);
+
+	// init
+	int i; 
+	for(i = 0; i < MAX_NUM; i++)
+		v->data[i] = i;
+}
+
+int main()
+{
+	printf("This is the optimized version .. \n");
+
+	vec_t info; 
+	int result; 
+	// init 
+	init(&info);
+
+	// combine 
+	combine(&info, &result);
+	//printf("combined val = %d\n", result);
+
+	return 0;
+}
